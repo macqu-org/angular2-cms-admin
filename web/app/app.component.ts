b50f181e40d55f6ea4aca134d68e971679f43ad0
wangliu <wangliu@isoftstone.com>
2017-03-13 22:05:27 +0800

    project init

---
+++ web/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-root',
+    template: '<h1>H1</h1>',
+})
+export class AppComponent {
+    name: string = "success!";
+}
