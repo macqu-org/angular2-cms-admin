6e295733d090e3194db2cbffa2e95439bde1aa5a
wangliu <wangliu@isoftstone.com>
2017-03-16 13:39:47 +0800

    添加共享module

--- web/app/pages/about/about.module.ts
+++ web/app/pages/about/about.module.ts
@@ -3,9 +3,10 @@ import {CommonModule} from "@angular/common";
 
 import {AboutRoutingModule} from './about.route.module';
 import {AboutPageComponent} from './about.component';
+import {ShareModule} from "../shared/share.module";
 
 @NgModule({
-    imports: [CommonModule, AboutRoutingModule],
+    imports: [CommonModule,ShareModule, AboutRoutingModule],
     declarations: [AboutPageComponent]
 })
 
--- web/app/pages/home/home.module.ts
+++ web/app/pages/home/home.module.ts
@@ -3,9 +3,10 @@ import {CommonModule} from "@angular/common";
 
 import {HomePageComponent} from './home.component';
 import {HomeRoutingModule} from './home.route.module';
+import {ShareModule} from "../shared/share.module";
 
 @NgModule({
-    imports: [CommonModule, HomeRoutingModule],
+    imports: [CommonModule, ShareModule, HomeRoutingModule],
     declarations: [HomePageComponent]
 })
 
