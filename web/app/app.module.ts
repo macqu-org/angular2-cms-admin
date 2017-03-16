143a2d57f052393f3a24bcae65294666c0d098ba
wangliu <wangliu@isoftstone.com>
2017-03-16 11:22:12 +0800

    home模块修改成模块懒加载的形式

--- web/app/app.module.ts
+++ web/app/app.module.ts
@@ -1,22 +1,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {SidebarModule} from 'ng-sidebar';
-
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app.route.module';
 
 @NgModule({
-    imports: [BrowserModule, AppRoutingModule, SidebarModule, NgbModule.forRoot()],
+    imports: [BrowserModule, AppRoutingModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
 
 export class AppModule {
-    private _opened: boolean = false;
-
-    private _toggleSidebar() {
-        this._opened = !this._opened;
-    }
 }
--- web/app/app.route.module.ts
+++ web/app/app.route.module.ts
@@ -2,13 +2,11 @@ import {NgModule} from "@angular/core";
 import {APP_BASE_HREF} from '@angular/common';
 
 import {Routes, RouterModule} from "@angular/router";
-import {HomePageComponent} from './pages/home/home.component';
 
-export const routes: Routes = [
-
+const routes: Routes = [
     {
         path: 'home',
-        component: HomePageComponent
+        loadChildren: 'app/pages/home/home.module#HomeModule'
     },
     {
         path: 'about',
@@ -21,13 +19,12 @@ export const routes: Routes = [
     },
     {
         path: '**',
-        component: HomePageComponent
+        loadChildren: 'app/pages/home/home.module#HomeModule'
     }
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes, {useHash: true})],
-    declarations: [HomePageComponent],
     providers: [{provide: APP_BASE_HREF, useValue: '/'}],
     exports: [RouterModule]
 })
