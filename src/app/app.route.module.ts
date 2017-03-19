57ffddaa73f458ab04307b095fcb5518ac4a1edc
wangliu <wangliu@isoftstone.com>
2017-03-19 20:36:17 +0800

    删除无用的路由地址

--- src/app/app.route.module.ts
+++ src/app/app.route.module.ts
@@ -2,27 +2,8 @@ import {NgModule} from "@angular/core";
 import {APP_BASE_HREF} from '@angular/common';
 
 import {Routes, RouterModule} from "@angular/router";
-import {HomePageComponent} from "./pages/home/home.component";
 
-const routes: Routes = [
-    // {
-    //     path: 'home',
-    //     component: HomePageComponent
-    // }
-    // {
-    //     path: 'about',
-    //     loadChildren: './pages/about/about.module#AboutModule'
-    // },
-    // {
-    //     path: '',
-    //     redirectTo: 'home',
-    //     pathMatch: 'full'
-    // },
-    // {
-    //     path: '**',
-    //     loadChildren: './pages/home/home.module#HomeModule'
-    // }
-];
+const routes: Routes = [];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes, {useHash: true})],
